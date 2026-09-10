16273b13480cd1a9f350617c88482956baec332b
berdibaevich <berdibaevich.me@gmail.com>
2026-09-10 14:32:27 +0500

    iterator: implement map using generator

--- iterator_labs/map.py
+++ iterator_labs/map.py
@@ -1,5 +1,6 @@
 from collections.abc import Iterable
 
+
 class Map:
     def __init__(self, func, *iterables):
         self._validate(func, iterables)
@@ -26,6 +27,21 @@ class Map:
     
 
 
+def map_(func, *iterables):
+    iterators = [iter(seq) for seq in iterables]
+
+    while True:
+        values = []
+        for iterator in iterators:
+            try:
+                values.append(next(iterator))
+            except StopIteration:
+                return
+        yield func(*values)
+
+
+
+
 def test_map_1():
     colors = ['red', 'yellow', 'green']
 
@@ -37,6 +53,18 @@ def test_map_1():
 
 
 
+def test_map_2():
+    colors = ['red', 'yellow', 'green']
+    numbers = [1, 2, 3, 4, 5]
+    hello = "Hello"
+
+    real_map = list(map(lambda *agrs: agrs, colors, numbers, hello))
+    own_map = list(map_(lambda *agrs: agrs, colors, numbers, hello))
+
+    assert real_map == own_map, "Test-2: Failed!"
+    print("Test-2: Passed!")
+
 
 if __name__ == "__main__":
     test_map_1()
+    test_map_2()
