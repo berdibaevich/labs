823cea1d2944aa1ce7cf2e5bb37481c4568601d1
berdibaevich <berdibaevich.me@gmail.com>
2026-09-14 22:24:22 +0500

    iterator(batched): feature 'strict' parameter

--- iterator_labs/batched.py
+++ iterator_labs/batched.py
@@ -1,9 +1,10 @@
 
 
 class Batched:
-    def __init__(self, iterable, n: int):
+    def __init__(self, iterable, n: int, *, strict=False):
         self._iterator = iter(iterable)
         self._n = n
+        self._strict = strict
 
     def __iter__(self):
         return self
@@ -14,8 +15,10 @@ class Batched:
             try:
                 values.append(next(self._iterator))
             except StopIteration:
-                if values:
-                    break
-                raise StopIteration
+                if not values:
+                    raise StopIteration
+                elif self._strict:
+                    raise ValueError
+                break
         return tuple(values)
     
