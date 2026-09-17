fa7f632c33dacdf0aaa7ed7901bc993efbc27e41
berdibaevich <berdibaevich.me@gmail.com>
2026-09-17 13:33:09 +0500

    del .gitignore

--- notification_system/apps/account/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Account, UploadToAvatar
-
-@admin.register(Account)
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'is_active', 'is_staff', 'is_superuser')
-
-
-admin.site.register(UploadToAvatar)
--- notification_system/apps/account/utils.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def get_upload_avatar_for_user():
-    guess = random.randrange(1, 5)
-    return f"avatars/avatar_{guess}.png"
