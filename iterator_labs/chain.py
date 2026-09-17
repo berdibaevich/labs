f87d23ddd490236f74a348501a451f0c58b961be
berdibaevich <berdibaevich.me@gmail.com>
2026-09-17 21:58:45 +0500

    chain: implement using iter and next dunder methods

--- iterator_labs/chain.py
+++ iterator_labs/chain.py
@@ -1,5 +1,29 @@
 
 
+class Chain:
+    def __init__(self, *iterables):
+        self._iterables = iterables
+        self._index = 0
+        self._current_iter = None
+        self._length = len(self._iterables)
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        while True:
+            if self._current_iter is None:
+                self._current_iter = iter(self._iterables[self._index])
+            try:
+                return next(self._current_iter)
+            except StopIteration:
+                if (index := self._index + 1) < self._length:
+                    self._current_iter = None
+                    self._index = index
+                    continue
+                raise 
+
+
 def chain_(*iterables):
     for iterable in iterables:
         yield from iterable
