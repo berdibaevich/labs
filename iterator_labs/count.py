0b02cc06aa960c6618958139a1a34e675750ce1b
berdibaevich <berdibaevich.me@gmail.com>
2026-09-18 17:35:41 +0500

    count: implement using Class

---
+++ iterator_labs/count.py
@@ -0,0 +1,14 @@
+
+
+class Count:
+    def __init__(self, start=0, step=1):
+        self._current = start
+        self._step = step
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        val = self._current
+        self._current += self._step
+        return val
